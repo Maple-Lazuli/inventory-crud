7dff6634967901d7c363e8b5303551186014fb04
Ada <the.nostra.tymus@gmail.com>
2023-06-23 11:36:13 -0400

    updated the interactions to add, delete, update, and get inventory items

--- backend/src/db_interactions.py
+++ backend/src/db_interactions.py
@@ -135,13 +135,47 @@ class DBInteractions:
             # log failure
 
     def add_item(self, account_id, name, description, quantity):
-        pass
+        try:
+            with self.connection, self.connection.cursor() as cur:
+                cur.execute(f"""INSERT INTO items (account_id, name, description, quantity, creation_date)
+                 VALUES (%(account_id)s,%(name)s,%(description)s,%(quantity)s,%(creation_date)s);""",
+                            {'account_id': account_id, 'name': name, 'description': description, 'quantity': quantity,
+                             'creation_date': datetime.now()})
 
-    def get_items(self, account_id = None):
-        pass
+            return True
+
+        except Exception as e:
+            return None
+            # log failure
+
+    def get_items(self, item_id=None):
+        try:
+            with self.connection, self.connection.cursor() as cur:
+                cur.execute(f"select * from items where item_id = %(item_id)s;",
+                            {'item_id': item_id})
+
+            return cur.fetchone()
+
+        except Exception as e:
+            return None
 
     def delete_item(self, item_id):
-        pass
+        try:
+            with self.connection, self.connection.cursor() as cur:
+                cur.execute(f"delete * from items where item_id = %s;", item_id)
+
+            return True
+
+        except Exception as e:
+            return False
 
     def update_item(self, item_id, name, description, quantity):
-        pass
+        try:
+            with self.connection, self.connection.cursor() as cur:
+                cur.execute(f"""update items SET name = %(name)s, description = %(description)s, 
+                quantity = %(quantity)s, modification_date = %(modification_date)s, where item_id = %(item_id)s;""",
+                            {'name': name, 'description': description, 'quantity': quantity,
+                             'modification_date': datetime.now(),'item_id': item_id})
+        except Exception as e:
+            return False
+            # log failure
